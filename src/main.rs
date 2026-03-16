286ce26a76be69f51bac54a7f20e3f8b7c5a2850
twilight0963 <ds.130206@gmail.com>
2026-03-16 19:28:34 +0530

    Rudimentary frame handling

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,41 @@
 mod cells; // Contains conway's game of life next frame gen for 2d vector
 
+use std::io;
 use std::thread;
-use std::time::Duration;
-
+use std::time::Duration; // Frame timers
+use crossterm::{execute, cursor::Hide, terminal}; // Handle TUI and cursor hide
 
 fn main() {
-    // DEBUG: Start with 5x5 vector with an oscillator
-    // print oscillator frames using os and whitespaces for now
-    let mut start:Vec<Vec<bool>> = vec![vec![false;5];5];
-    start[0][3] = true;
-    start[1][3] = true;
-    start[2][3] = true;
+    // Try to hide cursor
+    execute!(io::stdout(), Hide).expect("Could not hide cursor...");
+    
+    // Get terminal size
+    let (x,y) = terminal::size().expect("Terminal size not detected!\n");
+    let x_usize = x as usize;
+    let y_usize = y as usize;
 
-    loop {
-        for i in 0..5 {
-            for j in 0..5 {
-                if start[i][j] {
-                    print!("o");
-                } else {
-                    print!(" ");
-                }
-            }
-            print!("\n");
+
+    // Fill 2D vector with random boolean values
+    let mut start:Vec<Vec<bool>> = vec![vec![false; x_usize];y_usize];
+    for i in 0..y_usize {
+        for j in 0..x_usize {
+            start[i][j] = rand::random_bool(0.5);
         }
-        print!("\n");
-
-        start = cells::calculate_next(start);
-        thread::sleep(Duration::from_millis(500));
     }
+
+    // Multithread loop through frames.
+    let render_thread = thread::spawn(move || {
+        loop {
+            print!("\x1B[2J");
+            for i in 0..y_usize {
+                let buf = start[i].iter().map(|&v| if v {"█"} else {" "}).collect::<String>();
+                print!("{}\n",buf);
+            }
+            start = cells::calculate_next(start);
+            thread::sleep(Duration::from_millis(100));
+        }
+    });
+
+    // Join thread
+    render_thread.join().expect("Failed to join render thread");
 }
