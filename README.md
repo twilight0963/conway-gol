098cfe62fa960f39529671e44b3770c841c68167
twilight0963 <ds.130206@gmail.com>
2026-03-19 16:48:11 +0530

    Argument Support added

--- README.md
+++ README.md
@@ -15,10 +15,20 @@ Conway's game of life implemented within rust
 game-of-life
 ```
 
+### You can also specify a chance of a cell initially being populated:
+
+```sh
+# To give a 50% chance of a cell being alive on initial frame:
+game-of-life 0.5
+```
+
+**Note**: Value for initial population chance must be between 0 and 1.
+
 Exit by pressing `q`!
+
+## Screenshot:
 
 --- 
 
 ![Screenshot](./ss.png "Screenshot")
-
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod cells; // Contains conway's game of life next frame gen for 2d vector
 
-use std::io;
+use std::{env::args, io};
 use futures::{StreamExt, select, future::FutureExt};
 use futures_timer::Delay;
 use crossterm::{
@@ -10,10 +10,39 @@ use std::{time::Duration};
 
 #[tokio::main]
 async fn main() {
-    const FPS:u64 = 24; // Frames per second for cell rendering.
-    const INIT_POPULATION:f64 = 0.1; // Starting chance of a cell being active.
+    const FPS:u64 = 24; // Frames per second for cell rendering
+    const FRAMETIME:u64 = 1000/FPS;
+    let init_population:f64;
+
+    // Use program args for initial population chance, default = 0.1
+    let mut args = args().skip(1); 
+    match args.next() {
+        Some(population_str) => match population_str.parse::<f64>() {
+            Ok(n) => {
+                // Chance must be within 0 and 1, inclusive
+                if n <= 0.0 {
+                    eprintln!("Population cannot be negative or zero! Defaulting to 0.1");
+                    init_population = 0.1;
+                } else if n >= 1.0 {
+                    eprintln!("Population cannot exceed 1.0! Defaulting to 0.1");
+                    init_population = 0.1;
+                } else {
+                    init_population = n;
+                }
+            },
+            Err(_) => {
+                // Non-f64 value given
+                eprintln!("Population must be a floating integer in range 0-1! Defaulting to 0.1");
+                    init_population = 0.1;
+            }
+        }
+        // No argument given
+        None => init_population = 0.1,
+    }
+
     let mut stdout = io::stdout();
-    // Try to hide cursor
+
+    // Adjustments for input ignore and clean display.
     if enable_raw_mode().is_err() {
     }
     if execute!(stdout,terminal::EnterAlternateScreen).is_err() {
@@ -32,22 +61,15 @@ async fn main() {
     let mut start:Vec<Vec<bool>> = vec![vec![false; x_usize];y_usize];
     for i in 0..y_usize {
         for j in 0..x_usize {
-            start[i][j] = rand::random_bool(INIT_POPULATION);
+            start[i][j] = rand::random_bool(init_population);
         }
     }
 
-    tokio::spawn(async move {
-    });
-
-    const FRAMETIME:u64 = 1000/FPS;
     // Multithread loop through frames.
     tokio::spawn(async move {
         loop {
             let mut neighbors = cells::get_all_neighbors(&start,x_usize,y_usize);
             // Clear screen before starting print.
-            if execute!(stdout, terminal::Clear(terminal::ClearType::All)).is_err() {
-                eprintln!("Unable to clear terminal!");
-            }
 
             // Print by line
             for i in 0..y {
@@ -57,8 +79,10 @@ async fn main() {
                 }// Queue to print.
             }
 
-            // Start calculating next and wait atleast 70ms
+            // Start calculating next and wait
             neighbors = cells::calculate_next(&mut start,neighbors,y_usize,x_usize);
+
+            // Check for any exit signal while waiting
             let mut reader = EventStream::new();
             let mut delay = Delay::new(Duration::from_millis(FRAMETIME)).fuse();
             let mut event = reader.next().fuse();
@@ -67,6 +91,7 @@ async fn main() {
                 maybe_event = event => {
                     match maybe_event {
                         Some(Ok(event)) => {
+                            // Exit on pressing q
                             if event == Event::Key(KeyCode::Char('q').into()) {
                                 if execute!(stdout,terminal::LeaveAlternateScreen).is_err() {
                                     eprintln!("Unable to enter alternate screen");
